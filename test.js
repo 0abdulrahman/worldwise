3a35139d3afa4b1b3012fa51bebf3115b49f2d1d
7saru <7saru@users.noreply.github.com>
2023-08-18 22:54:48 +0000

    deploy: bf57d759164f5ddc22d111dc5a45c6dc43ce2a17

---
+++ test.js
@@ -0,0 +1,83 @@
+const compounds = [
+  {
+    compoundName: "ZED East in New Cairo",
+    startPrice: "9,840,000",
+    maxPrice: "23,518,000",
+    area: "new giza",
+    country: "egypt",
+    properties: ["Apartment", "Villa"],
+    details: {
+      gallery: ["data:img...", "data:img...", "data:img...", "data:img...", "data:img..."],
+      location: [24.454645, 68.454645],
+      plan: "data:img...",
+    },
+    facilities: ["Clubhouse", "Schools", "Commercial Strip", "Business Hub", "Bicycles Lanes"],
+    paymentPlans: {
+      plan1: {
+        percent: "5%",
+        years: 8,
+      },
+      plan2: {
+        percent: "10%",
+        years: 5,
+      },
+    },
+    about: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis quae deserunt quasi corrupti aut quod nostrum rerum nisi fugiat porro, nemo totam iure animi error ducimus explicabo officia debitis pariatur ipsam quas.`,
+  },
+];
+
+const properties = [
+  {
+    compoundName: "ZED East in New Cairo",
+    startPrice: "9,840,000",
+    maxPrice: "23,518,000",
+    area: "new giza",
+    country: "egypt",
+    properties: ["Apartment", "Villa"],
+    details: {
+      gallery: ["data:img...", "data:img...", "data:img...", "data:img...", "data:img..."],
+      location: [24.454645, 68.454645],
+      plan: "data:img...",
+    },
+    facilities: ["Clubhouse", "Schools", "Commercial Strip", "Business Hub", "Bicycles Lanes"],
+    paymentPlans: {
+      plan1: {
+        percent: "5%",
+        years: 8,
+      },
+      plan2: {
+        percent: "10%",
+        years: 5,
+      },
+    },
+    about: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis quae deserunt quasi corrupti aut quod nostrum rerum nisi fugiat porro, nemo totam iure animi error ducimus explicabo officia debitis pariatur ipsam quas.`,
+  },
+];
+
+const areas = [
+  {
+    compoundName: "ZED East in New Cairo",
+    startPrice: "9,840,000",
+    maxPrice: "23,518,000",
+    area: "new giza",
+    country: "egypt",
+    properties: ["Apartment", "Villa"],
+    details: {
+      gallery: ["data:img...", "data:img...", "data:img...", "data:img...", "data:img..."],
+      location: [24.454645, 68.454645],
+      plan: "data:img...",
+    },
+    facilities: ["Clubhouse", "Schools", "Commercial Strip", "Business Hub", "Bicycles Lanes"],
+    paymentPlans: {
+      plan1: {
+        percent: "5%",
+        years: 8,
+      },
+      plan2: {
+        percent: "10%",
+        years: 5,
+      },
+    },
+    about: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis quae deserunt quasi corrupti aut quod nostrum rerum nisi fugiat porro, nemo totam iure animi error ducimus explicabo officia debitis pariatur ipsam quas.`,
+  },
+];
