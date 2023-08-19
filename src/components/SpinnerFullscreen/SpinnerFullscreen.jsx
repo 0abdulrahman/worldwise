a6f423e124a243baa3a4ae12b0e51562b68dc6fd
7saru <saboo9653@gmail.com>
2023-08-19 16:40:30 +0300

    adding lazy load

---
+++ src/components/SpinnerFullscreen/SpinnerFullscreen.jsx
@@ -0,0 +1,11 @@
+import styles from "./SpinnerFullscreen.module.css";
+
+function SpinnerFullscreen() {
+  return (
+    <div className={styles.spinnerFullscreen}>
+      <span></span>
+    </div>
+  );
+}
+
+export default SpinnerFullscreen;
