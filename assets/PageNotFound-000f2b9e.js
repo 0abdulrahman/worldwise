356322dd5ad9e84052cce24d5b458af2936fff97
7saru <7saru@users.noreply.github.com>
2023-08-19 13:41:17 +0000

    deploy: a6f423e124a243baa3a4ae12b0e51562b68dc6fd

---
+++ assets/PageNotFound-000f2b9e.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-e1603e4d.js";function e(){return n.jsx(n.Fragment,{children:n.jsx("h2",{children:"Page not found :()"})})}export{e as default};
---
+++ assets/Product-6dad1312.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-e1603e4d.js";function e(){return r.jsx(r.Fragment,{children:r.jsx("h2",{children:"Product"})})}export{e as default};
